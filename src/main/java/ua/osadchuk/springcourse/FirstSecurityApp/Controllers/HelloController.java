f278ee6daefad75311a13e491d48925b919da1fb
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-02-09 16:04:40 +0200

    Initial commit

---
+++ src/main/java/ua/osadchuk/springcourse/FirstSecurityApp/Controllers/HelloController.java
@@ -0,0 +1,24 @@
+package ua.osadchuk.springcourse.FirstSecurityApp.Controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import ua.osadchuk.springcourse.FirstSecurityApp.security.PersonDetails;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHallo() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
+    }
+}
